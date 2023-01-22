7265c0d1ed80edaaf29c14666d166d9b1d4a3c50
Ravi Shankar Jha <ravi.jha3369@gmail.com>
2023-01-22 19:03:11 +0530

    image fix

--- src/Footer/footer.jsx
+++ src/Footer/footer.jsx
@@ -5,11 +5,11 @@ const footer = ()=>{
     return (
         <div className={css.foot}>
             <span>
-                <a target="_blank" href='https://github.com/Ravijhabit/Clock-Calculator'><img src='./../../asset/git.svg' alt='github'/></a>
+                <a target="_blank" href='https://github.com/Ravijhabit/Clock-Calculator'><img src='./asset/git.svg' alt='github'/></a>
             </span>
             <h3>Designed and Developed in ❤️ with ⏰</h3>
             <span>
-                <a target="_blank" href='https://www.figma.com/community/file/1187850020481497519'><img src='./../../asset/figma.svg' alt='figma'/></a>
+                <a target="_blank" href='https://www.figma.com/community/file/1187850020481497519'><img src='./asset/figma.svg' alt='figma'/></a>
             </span>
         </div>
     )
